0b7133dcb080ede1c4ddf32992e79c12c6156194
Waty <wartaarnoutse@gmail.com>
2014-11-04 11:00:45 +0100

    Tested the program and added a listview

--- sWasteController/Form1.Designer.cs
+++ sWasteController/Form1.Designer.cs
@@ -35,30 +35,37 @@
             this.cbSerialPorts = new System.Windows.Forms.ComboBox();
             this.lAvaillablePorts = new System.Windows.Forms.Label();
             this.statusStrip = new System.Windows.Forms.StatusStrip();
-            this.tslConnected = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslConnected = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tsslStatus = new System.Windows.Forms.ToolStripStatusLabel();
             this.gbControls = new System.Windows.Forms.GroupBox();
-            this.lbLogs = new System.Windows.Forms.ListBox();
+            this.lTotalCount = new System.Windows.Forms.Label();
+            this.lTotal = new System.Windows.Forms.Label();
+            this.lvResults = new System.Windows.Forms.ListView();
+            this.chCount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chColor = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.bStop = new System.Windows.Forms.Button();
             this.bStart = new System.Windows.Forms.Button();
             this.tReceiveMessages = new System.Windows.Forms.Timer(this.components);
-            this.tsslRed = new System.Windows.Forms.ToolStripStatusLabel();
-            this.tsslWhite = new System.Windows.Forms.ToolStripStatusLabel();
-            this.tsslBlack = new System.Windows.Forms.ToolStripStatusLabel();
+            this.cmsTrash = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tsmiProcessWaste = new System.Windows.Forms.ToolStripMenuItem();
             this.gbConnection.SuspendLayout();
             this.statusStrip.SuspendLayout();
             this.gbControls.SuspendLayout();
+            this.cmsTrash.SuspendLayout();
             this.SuspendLayout();
             // 
             // gbConnection
             // 
+            this.gbConnection.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.gbConnection.Controls.Add(this.bDisconnect);
             this.gbConnection.Controls.Add(this.bConnect);
             this.gbConnection.Controls.Add(this.cbSerialPorts);
             this.gbConnection.Controls.Add(this.lAvaillablePorts);
             this.gbConnection.Location = new System.Drawing.Point(16, 15);
-            this.gbConnection.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbConnection.Margin = new System.Windows.Forms.Padding(4);
             this.gbConnection.Name = "gbConnection";
-            this.gbConnection.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbConnection.Padding = new System.Windows.Forms.Padding(4);
             this.gbConnection.Size = new System.Drawing.Size(347, 87);
             this.gbConnection.TabIndex = 0;
             this.gbConnection.TabStop = false;
@@ -68,7 +75,7 @@
             // 
             this.bDisconnect.Enabled = false;
             this.bDisconnect.Location = new System.Drawing.Point(8, 49);
-            this.bDisconnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bDisconnect.Margin = new System.Windows.Forms.Padding(4);
             this.bDisconnect.Name = "bDisconnect";
             this.bDisconnect.Size = new System.Drawing.Size(147, 28);
             this.bDisconnect.TabIndex = 1;
@@ -78,8 +85,10 @@
             // 
             // bConnect
             // 
+            this.bConnect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.bConnect.Location = new System.Drawing.Point(163, 49);
-            this.bConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bConnect.Margin = new System.Windows.Forms.Padding(4);
             this.bConnect.Name = "bConnect";
             this.bConnect.Size = new System.Drawing.Size(176, 28);
             this.bConnect.TabIndex = 2;
@@ -89,9 +98,11 @@
             // 
             // cbSerialPorts
             // 
+            this.cbSerialPorts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.cbSerialPorts.FormattingEnabled = true;
             this.cbSerialPorts.Location = new System.Drawing.Point(163, 16);
-            this.cbSerialPorts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbSerialPorts.Margin = new System.Windows.Forms.Padding(4);
             this.cbSerialPorts.Name = "cbSerialPorts";
             this.cbSerialPorts.Size = new System.Drawing.Size(175, 24);
             this.cbSerialPorts.TabIndex = 1;
@@ -109,10 +120,8 @@
             // statusStrip
             // 
             this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.tslConnected,
-            this.tsslRed,
-            this.tsslWhite,
-            this.tsslBlack});
+            this.tsslConnected,
+            this.tsslStatus});
             this.statusStrip.Location = new System.Drawing.Point(0, 296);
             this.statusStrip.Name = "statusStrip";
             this.statusStrip.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
@@ -120,42 +129,93 @@
             this.statusStrip.TabIndex = 1;
             this.statusStrip.Text = "statusStrip1";
             // 
-            // tslConnected
+            // tsslConnected
             // 
-            this.tslConnected.ForeColor = System.Drawing.Color.Red;
-            this.tslConnected.Name = "tslConnected";
-            this.tslConnected.Size = new System.Drawing.Size(112, 20);
-            this.tslConnected.Text = "Niet verbonden";
+            this.tsslConnected.ForeColor = System.Drawing.Color.Red;
+            this.tsslConnected.Name = "tsslConnected";
+            this.tsslConnected.Size = new System.Drawing.Size(112, 20);
+            this.tsslConnected.Text = "Niet verbonden";
+            // 
+            // tsslStatus
+            // 
+            this.tsslStatus.Name = "tsslStatus";
+            this.tsslStatus.Size = new System.Drawing.Size(43, 20);
+            this.tsslStatus.Text = "Idle...";
             // 
             // gbControls
             // 
-            this.gbControls.Controls.Add(this.lbLogs);
+            this.gbControls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gbControls.Controls.Add(this.lTotalCount);
+            this.gbControls.Controls.Add(this.lTotal);
+            this.gbControls.Controls.Add(this.lvResults);
             this.gbControls.Controls.Add(this.bStop);
             this.gbControls.Controls.Add(this.bStart);
             this.gbControls.Enabled = false;
             this.gbControls.Location = new System.Drawing.Point(16, 110);
-            this.gbControls.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gbControls.Margin = new System.Windows.Forms.Padding(4);
             this.gbControls.Name = "gbControls";
-            this.gbControls.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gbControls.Size = new System.Drawing.Size(347, 181);
+            this.gbControls.Padding = new System.Windows.Forms.Padding(4);
+            this.gbControls.Size = new System.Drawing.Size(347, 182);
             this.gbControls.TabIndex = 2;
             this.gbControls.TabStop = false;
             this.gbControls.Text = "Controls";
             // 
-            // lbLogs
+            // lTotalCount
             // 
-            this.lbLogs.FormattingEnabled = true;
-            this.lbLogs.ItemHeight = 16;
-            this.lbLogs.Location = new System.Drawing.Point(8, 59);
-            this.lbLogs.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.lbLogs.Name = "lbLogs";
-            this.lbLogs.Size = new System.Drawing.Size(329, 116);
-            this.lbLogs.TabIndex = 2;
+            this.lTotalCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lTotalCount.AutoSize = true;
+            this.lTotalCount.Location = new System.Drawing.Point(65, 161);
+            this.lTotalCount.Name = "lTotalCount";
+            this.lTotalCount.Size = new System.Drawing.Size(16, 17);
+            this.lTotalCount.TabIndex = 4;
+            this.lTotalCount.Text = "0";
+            // 
+            // lTotal
+            // 
+            this.lTotal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.lTotal.AutoSize = true;
+            this.lTotal.Location = new System.Drawing.Point(7, 161);
+            this.lTotal.Name = "lTotal";
+            this.lTotal.Size = new System.Drawing.Size(52, 17);
+            this.lTotal.TabIndex = 3;
+            this.lTotal.Text = "Totaal:";
+            // 
+            // lvResults
+            // 
+            this.lvResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chCount,
+            this.chColor});
+            this.lvResults.ContextMenuStrip = this.cmsTrash;
+            this.lvResults.FullRowSelect = true;
+            this.lvResults.GridLines = true;
+            this.lvResults.Location = new System.Drawing.Point(7, 58);
+            this.lvResults.Name = "lvResults";
+            this.lvResults.Size = new System.Drawing.Size(333, 100);
+            this.lvResults.TabIndex = 2;
+            this.lvResults.UseCompatibleStateImageBehavior = false;
+            this.lvResults.View = System.Windows.Forms.View.Details;
+            // 
+            // chCount
+            // 
+            this.chCount.Text = "#";
+            this.chCount.Width = 20;
+            // 
+            // chColor
+            // 
+            this.chColor.Text = "Soort afval";
+            this.chColor.Width = 250;
             // 
             // bStop
             // 
+            this.bStop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.bStop.Location = new System.Drawing.Point(160, 23);
-            this.bStop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bStop.Margin = new System.Windows.Forms.Padding(4);
             this.bStop.Name = "bStop";
             this.bStop.Size = new System.Drawing.Size(179, 28);
             this.bStop.TabIndex = 1;
@@ -166,7 +226,7 @@
             // bStart
             // 
             this.bStart.Location = new System.Drawing.Point(8, 23);
-            this.bStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bStart.Margin = new System.Windows.Forms.Padding(4);
             this.bStart.Name = "bStart";
             this.bStart.Size = new System.Drawing.Size(140, 28);
             this.bStart.TabIndex = 0;
@@ -178,23 +238,19 @@
             // 
             this.tReceiveMessages.Tick += new System.EventHandler(this.tReceiveMessages_Tick);
             // 
-            // tsslRed
+            // cmsTrash
             // 
-            this.tsslRed.Name = "tsslRed";
-            this.tsslRed.Size = new System.Drawing.Size(60, 20);
-            this.tsslRed.Text = "Rood: 0";
+            this.cmsTrash.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiProcessWaste});
+            this.cmsTrash.Name = "cmsTrash";
+            this.cmsTrash.Size = new System.Drawing.Size(176, 56);
             // 
-            // tsslWhite
+            // tsmiProcessWaste
             // 
-            this.tsslWhite.Name = "tsslWhite";
-            this.tsslWhite.Size = new System.Drawing.Size(47, 20);
-            this.tsslWhite.Text = "Wit: 0";
-            // 
-            // tsslBlack
-            // 
-            this.tsslBlack.Name = "tsslBlack";
-            this.tsslBlack.Size = new System.Drawing.Size(62, 20);
-            this.tsslBlack.Text = "Zwart: 0";
+            this.tsmiProcessWaste.Name = "tsmiProcessWaste";
+            this.tsmiProcessWaste.Size = new System.Drawing.Size(175, 24);
+            this.tsmiProcessWaste.Text = "Verwerk Afval";
+            this.tsmiProcessWaste.Click += new System.EventHandler(this.tsmiProcessWaste_Click);
             // 
             // MainForm
             // 
@@ -204,7 +260,7 @@
             this.Controls.Add(this.gbControls);
             this.Controls.Add(this.statusStrip);
             this.Controls.Add(this.gbConnection);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.Text = "sWaste Bediening";
             this.gbConnection.ResumeLayout(false);
@@ -212,6 +268,8 @@
             this.statusStrip.ResumeLayout(false);
             this.statusStrip.PerformLayout();
             this.gbControls.ResumeLayout(false);
+            this.gbControls.PerformLayout();
+            this.cmsTrash.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -225,15 +283,19 @@
         private System.Windows.Forms.Button bDisconnect;
         private System.Windows.Forms.Button bConnect;
         private System.Windows.Forms.StatusStrip statusStrip;
-        private System.Windows.Forms.ToolStripStatusLabel tslConnected;
+        private System.Windows.Forms.ToolStripStatusLabel tsslConnected;
         private System.Windows.Forms.GroupBox gbControls;
         private System.Windows.Forms.Button bStop;
         private System.Windows.Forms.Button bStart;
-        private System.Windows.Forms.ListBox lbLogs;
         private System.Windows.Forms.Timer tReceiveMessages;
-        private System.Windows.Forms.ToolStripStatusLabel tsslRed;
-        private System.Windows.Forms.ToolStripStatusLabel tsslWhite;
-        private System.Windows.Forms.ToolStripStatusLabel tsslBlack;
+        private System.Windows.Forms.ListView lvResults;
+        private System.Windows.Forms.ColumnHeader chCount;
+        private System.Windows.Forms.ColumnHeader chColor;
+        private System.Windows.Forms.ToolStripStatusLabel tsslStatus;
+        private System.Windows.Forms.Label lTotalCount;
+        private System.Windows.Forms.Label lTotal;
+        private System.Windows.Forms.ContextMenuStrip cmsTrash;
+        private System.Windows.Forms.ToolStripMenuItem tsmiProcessWaste;
     }
 }
 
--- sWasteController/Form1.cs
+++ sWasteController/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Drawing;
 using System.IO.Ports;
+using System.Linq;
 using System.Speech.Synthesis;
 using System.Windows.Forms;
 using EV3MessengerLib;
@@ -12,11 +13,11 @@ namespace sWasteController
         private const string LogicBox = "LogicBox";
         private readonly EV3Messenger _messenger = new EV3Messenger();
         private readonly SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
-        private int _black;
-        private int _red;
-        private int _white;
+        private int _trashCount;
 
-
+        /// <summary>
+        ///     Initializes every control on the form
+        /// </summary>
         public MainForm()
         {
             InitializeComponent();
@@ -27,56 +28,50 @@ namespace sWasteController
             if (cbSerialPorts.Items.Count > 0) cbSerialPorts.SelectedIndex = 0;
         }
 
-
-        // Updates the UI
+        /// <summary>
+        ///     Gets the IsConnected state of the EV3Messenger
+        /// </summary>
         public bool Connected
         {
             get { return _messenger.IsConnected; }
-            set
-            {
-                tslConnected.ForeColor = value ? Color.Green : Color.Red;
-                tslConnected.Text = value ? String.Format("Verbonden met {0}", _messenger.Port) : "Niet Verbonden";
-                gbControls.Enabled = value;
-                bConnect.Enabled = !value;
-                bDisconnect.Enabled = value;
-
-                tReceiveMessages.Enabled = value;
-            }
         }
 
-        public int Red
+        /// <summary>
+        ///     Keeps track of the amount of trash currently sits in the depot
+        /// </summary>
+        public int TrashCount
         {
-            get { return _red; }
+            get { return _trashCount; }
             set
             {
-                _red = value;
-                tsslRed.Text = "Rood: " + _red;
+                _trashCount = value;
+                lTotalCount.Text = value.ToString("G");
             }
         }
 
-        public int Black
+        /// <summary>
+        ///     Updates the UI to reflect the connected state
+        /// </summary>
+        /// <param name="connected">If the UI should look connected or not</param>
+        public void UpdateUi(bool connected)
         {
-            get { return _black; }
-            set
-            {
-                _black = value;
-                tsslBlack.Text = "Zwart: " + _black;
-            }
+            tsslConnected.ForeColor = connected ? Color.Green : Color.Red;
+            tsslConnected.Text = connected ? String.Format("Verbonden met {0}", _messenger.Port) : "Niet Verbonden";
+            gbControls.Enabled = connected;
+            bConnect.Enabled = !connected;
+            bDisconnect.Enabled = connected;
+
+            tReceiveMessages.Enabled = connected;
         }
 
-        public int White
-        {
-            get { return _white; }
-            set
-            {
-                _white = value;
-                tsslWhite.Text = "Wit: " + _white;
-            }
-        }
-
+        /// <summary>
+        ///     Connects to the EV3 using the EV3Messenger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bConnect_Click(object sender, EventArgs e)
         {
-            if (_messenger.Connect(cbSerialPorts.Text)) Connected = true;
+            if (_messenger.Connect(cbSerialPorts.Text)) UpdateUi(true);
             else
             {
                 MessageBox.Show(String.Format(
@@ -85,18 +80,34 @@ namespace sWasteController
             }
         }
 
+        /// <summary>
+        ///     Disconnects from the EV3 using the EV3Messenger
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bDisconnect_Click(object sender, EventArgs e)
         {
             _messenger.Disconnect();
-            Connected = false;
+            UpdateUi(false);
         }
 
+        /// <summary>
+        ///     The eventhandler for the start and stop button, if the sender is the start button it sends tru to LogicBox else it
+        ///     sends false
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bStartStop_Click(object sender, EventArgs e)
         {
             bool start = sender == bStart;
             _messenger.SendMessage(LogicBox, start);
         }
 
+        /// <summary>
+        ///     Checks and handles new BT Messages
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void tReceiveMessages_Tick(object sender, EventArgs e)
         {
             if (Connected)
@@ -107,36 +118,88 @@ namespace sWasteController
                 switch (message.MailboxTitle)
                 {
                     case "Color":
-                        HandleColorUpdate((Colors)message.ValueAsNumber);
+                        HandleColorUpdate((Colors) message.ValueAsNumber);
                         break;
 
                     default:
-                        lbLogs.Items.Add(string.Format("[{0}] {1}", DateTime.Now.ToShortTimeString(),
-                            message.ValueAsText));
+                        tsslStatus.Text = message.ValueAsText;
                         _synthesizer.SpeakAsync(message.ValueAsText);
                         break;
                 }
             }
-            else Connected = false;
+            else UpdateUi(false);
         }
 
+        /// <summary>
+        ///     Gets or creates the ListViewItem with the right color text
+        /// </summary>
+        /// <param name="color">The color that you want</param>
+        /// <returns>A ListViewItem with 2 SubItems: Count and Name</returns>
+        private ListViewItem GetListViewItem(Colors color)
+        {
+            return
+                lvResults.Items.Cast<ListViewItem>().FirstOrDefault(item => item.SubItems[1].Text == ColorToText(color)) ??
+                lvResults.Items.Add(new ListViewItem(new[] {"0", ColorToText(color)}));
+        }
+
+        /// <summary>
+        ///     Increments the Count of the ListViewItem that has the same name as the color
+        /// </summary>
+        /// <param name="color">The color that needs to be incremented</param>
         private void HandleColorUpdate(Colors color)
         {
-            _synthesizer.SpeakAsync(color + " detected");
+            ListViewItem listViewItem = GetListViewItem(color);
+            int count;
+            if (!Int32.TryParse(listViewItem.SubItems[0].Text, out count)) count = 0;
+            listViewItem.SubItems[0].Text = (count + 1).ToString("G");
+
+            TrashCount++;
+            _synthesizer.SpeakAsync("Processing " + ColorToText(color));
+            lvResults.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+
+        /// <summary>
+        ///     Returns the description of the specified color means
+        /// </summary>
+        /// <param name="color"></param>
+        /// <returns></returns>
+        private static string ColorToText(Colors color)
+        {
             switch (color)
             {
+                case Colors.White:
+                case Colors.Blue:
+                    return "Liquid waste";
+
                 case Colors.Red:
-                    Red++;
-                    break;
-                case Colors.Black:
-                    Black++;
-                    break;
+                    return "Explosive waste";
+
                 default:
-                    White++;
-                    break;
+                    return "Nuclear waste";
             }
         }
 
+        /// <summary>
+        ///     Removes the selected items from the ListView and decreases TrashCount
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void tsmiProcessWaste_Click(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection items = lvResults.SelectedItems;
+            if (items.Count <= 0) return;
+
+            foreach (ListViewItem item in items)
+            {
+                int count = Int32.Parse(item.SubItems[0].Text);
+                TrashCount -= count;
+                lvResults.Items.Remove(item);
+            }
+        }
+
+        /// <summary>
+        ///     All the possible colors that the EV3 can detect
+        /// </summary>
         private enum Colors
         {
             Default,
